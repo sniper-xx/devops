f4615eefa8c467aebb6f67fd52b74339fe3d52b7
sniper-xx <zhouxb009@126.com>
2019-11-18 12:00:00 +0800

    skaffold测试

--- src/main/java/org/sniper/cloud/devops/web/MainController.java
+++ src/main/java/org/sniper/cloud/devops/web/MainController.java
@@ -6,17 +6,23 @@
 package org.sniper.cloud.devops.web;
 
 import com.google.common.collect.ImmutableMap;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
+@Slf4j
 @RestController
 public class MainController {
 
     @GetMapping("/index")
     public Map<String,String> sayHello(@RequestParam(value = "name",required = false,defaultValue = "world")String name){
-        return ImmutableMap.of("name",name);
+        log.debug("user: {}", name);
+        return ImmutableMap.of("name",name,
+                "date", LocalDateTime.now().toString(),
+                "address","重庆市南岸区");
     }
 }
